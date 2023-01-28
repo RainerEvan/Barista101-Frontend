ff91eb332ddc79fbfa5a0d1238973904d11cf397
RainerEvan <rainerevan@gmail.com>
2023-01-29 01:12:04 +0700

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:'http://localhost:8080/api',
+  apiUrl:'https://barista101-api.herokuapp.com/api',
   firebase: {
     apiKey: "AIzaSyAiUPvxIBsajeVZy31uGXJJQ82iTsc2xus",
     authDomain: "barista101-c9d1e.firebaseapp.com",
@@ -10,5 +10,4 @@ export const environment = {
     appId: "1:422499097295:web:b7e75d90449822770cb5ba",
     measurementId: "G-WHKJL5TE8D"
   }
-  // apiUrl:'https://ce1c-2001-448a-2042-5d6d-cd24-412f-54c8-3fc9.ap.ngrok.io/api'
 };
