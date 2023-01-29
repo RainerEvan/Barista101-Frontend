8aa1fdabbc766ccc7ff31f21a3bdebb30405dd18
RainerEvan <rainerevan@gmail.com>
2023-01-29 22:13:26 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,6 @@ export const environment = {
     appId: "1:422499097295:web:b7e75d90449822770cb5ba",
     measurementId: "G-WHKJL5TE8D"
   }
-  // apiUrl:'https://ce1c-2001-448a-2042-5d6d-cd24-412f-54c8-3fc9.ap.ngrok.io/api',
 };
 
 /*
