36e13f26fc0534e91033ca670e5f24f5b124f705
RainerEvan <rainerevan@gmail.com>
2023-01-29 19:08:10 +0700

    menambahkan environment

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,14 +8,12 @@ declare namespace NodeJS {
     readonly NG_APP_ENV: string;
 
     NG_APP_API_URL: string;
-    NG_APP_FIREBASE: {
-      NG_APP_APIKEY: string,
-      NG_APP_AUTHDOMAIN: string,
-      NG_APP_PROJECTID: string,
-      NG_APP_STORAGEBUCKET: string,
-      NG_APP_MESSAGINGSENDERID: string,
-      NG_APP_APPID: string,
-      NG_APP_MEASUREMENTID: string,
-    };
+    NG_APP_APIKEY: string,
+    NG_APP_AUTHDOMAIN: string,
+    NG_APP_PROJECTID: string,
+    NG_APP_STORAGEBUCKET: string,
+    NG_APP_MESSAGINGSENDERID: string,
+    NG_APP_APPID: string,
+    NG_APP_MEASUREMENTID: string,
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   apiUrl:process.env.NG_APP_API_URL,
   firebase: {
-    apiKey: "AIzaSyAiUPvxIBsajeVZy31uGXJJQ82iTsc2xus",
-    authDomain: "barista101-c9d1e.firebaseapp.com",
-    projectId: "barista101-c9d1e",
-    storageBucket: "barista101-c9d1e.appspot.com",
-    messagingSenderId: "422499097295",
-    appId: "1:422499097295:web:b7e75d90449822770cb5ba",
-    measurementId: "G-WHKJL5TE8D"
+    apiKey: process.env.NG_APP_APIKEY,
+    authDomain: process.env.NG_APP_AUTHDOMAIN,
+    projectId: process.env.NG_APP_PROJECTID,
+    storageBucket: process.env.NG_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.NG_APP_MESSAGINGSENDERID,
+    appId: process.env.NG_APP_APPID,
+    measurementId: process.env.NG_APP_MEASUREMENTID
   }
 };
